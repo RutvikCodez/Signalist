f47e46307cca1c19e5fba378bde2d5b4aa27112c
RutvikCodez <rutvikdarji21@gnu.ac.in>
2025-12-31 21:00:41 +0530

    authtication basic pages and routes are created

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return <main className="min-h-screen text-gray-400"></main>;
+};
+
+export default AuthLayout;
