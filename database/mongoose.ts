5db988c3e31d44dc5cd39b46223a03ca91709d1f
RutvikCodez <rutvikdarji21@gnu.ac.in>
2025-12-31 22:51:19 +0530

    Mongo Db and mongoose integrated

---
+++ database/mongoose.ts
@@ -0,0 +1,30 @@
+const MONGODB_URI = process.env.MONGODB_URI;
+import mongoose from "mongoose";
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+let cached = global.mongooseCache;
+if (!cached) {
+  cached = global.mongooseCache = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+  if (!MONGODB_URI) throw new Error("MONGODB_URI must be within .env");
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+  console.log("Connected to daatbase");
+  return cached.conn;
+};
